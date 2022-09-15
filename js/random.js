48640487b0c13234af3b9f559119c37cf4733e04
sjhfuigashgrs <88381028+sjhfuigashgrs@users.noreply.github.com>
2022-09-15 15:37:32 +0000

    🎉 INT COMMIT; 添加/修改文章内容，修改分页按钮样式 Updated By Github Actions

--- js/random.js
+++ js/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/3eeb.html","posts/5c18.html","posts/85a2.html","posts/2753.html","posts/b417.html","posts/7d58.html","posts/18ac.html","posts/b427.html"];function toRandomPost(){window.open("/"+posts[Math.floor(Math.random()*posts.length)],"_self")}
+var posts=["posts/3eeb.html","posts/5c18.html","posts/85a2.html","posts/2753.html","posts/b417.html","posts/9968.html","posts/7d58.html","posts/18ac.html","posts/b427.html"];function toRandomPost(){window.open("/"+posts[Math.floor(Math.random()*posts.length)],"_self")}
